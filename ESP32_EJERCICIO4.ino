656c2aab33f6bb84958127b93ab37117e0b19da8
FranMil1000 <48313738@est.ort.edu.ar>
2025-03-25 15:56:13 -0300

    Add files via upload

---
+++ ESP32_EJERCICIO4.ino
@@ -0,0 +1,60 @@
+//MILSZTAIN_MACIA_SABIN_RUBINSTEIN
+//LIBRERIAS
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+//VARIABLES
+#define LED 25 //LED INTEGRADO
+#define SW1 34 
+#define SW2 35
+int umbral;
+
+//Dimensiones de la pantalla
+#define LONGITUD 128 // longitud
+#define ALTURA 64 // altura
+Adafruit_SSD1306 display(LONGITUD, ALTURA, &Wire, -1);
+
+//Pin sensor temp
+#define DHTPIN 23
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+
+  dht.begin(); //inicializa en sensor de temperatura
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //inicializa el display
+}
+
+void loop() 
+{
+  //Se lee la temperatura (en celcious)
+  float temp = dht.readTemperature();
+  
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.setTextSize(2); //Establece el tamaño de texto
+  display.setTextColor(WHITE);
+  display.println(temp); //display de temperatura
+  display.display(); //Muestra la temperatura
+  
+  Serial.println(temp);
+  delay(2000);
+
+  if (temp < 28)
+    {
+      digitalWrite(LED, LOW);
+    }
+
+  if (temp >= 28)
+  {
+    digitalWrite(LED, HIGH);
+  }
+}
