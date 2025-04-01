91d9655962107c68ac8cde2280ed0efdea547642
FranMil1000 <48313738@est.ort.edu.ar>
2025-04-01 14:46:08 -0300

    Update ESP32_EJERCICIO4.ino

--- ESP32_EJERCICIO4.ino
+++ ESP32_EJERCICIO4.ino
@@ -6,10 +6,19 @@
 #include <DHT.h>
 
 //VARIABLES
-#define LED 25 //LED INTEGRADO
-#define SW1 34 
-#define SW2 35
-int umbral;
+#define LED 25   //LED INTEGRADO
+float umbral = 28; //Temperatura de umbral
+int pantalla = 2;    //1 = temp actual y valor umbral / 2 = solo umbral (modificado)
+int cambiar = 0;
+
+//BOTONES
+#define SWAU 34 //switch de aumentar
+int valau;
+int bloqau = 0 ;
+
+#define SWRE 35 //switch de reducir
+int valre;
+int bloqre = 0;
 
 //Dimensiones de la pantalla
 #define LONGITUD 128 // longitud
@@ -24,10 +33,11 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() 
 {
   Serial.begin(9600);
+  Serial.println("iniciado");
 
   pinMode(LED, OUTPUT);
-  pinMode(SW1, INPUT);
-  pinMode(SW2, INPUT);
+  pinMode(SWAU, INPUT);
+  pinMode(SWRE, INPUT);
 
   dht.begin(); //inicializa en sensor de temperatura
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //inicializa el display
@@ -38,22 +48,97 @@ void loop()
   //Se lee la temperatura (en celcious)
   float temp = dht.readTemperature();
   
-  display.clearDisplay();
-  display.setCursor(0, 10);
-  display.setTextSize(2); //Establece el tamaño de texto
-  display.setTextColor(WHITE);
-  display.println(temp); //display de temperatura
-  display.display(); //Muestra la temperatura
-  
-  Serial.println(temp);
-  delay(2000);
+  //DISPLAY
+  if (pantalla == 1) //temp y umbral
+  {
+    display.clearDisplay();
+    display.setTextSize(2); //Establece el tamaño de texto
+    
+    display.setCursor(0, 10);
+    display.setTextColor(WHITE);
+    display.println(temp); //display de temperatura
 
-  if (temp < 28)
+    display.setCursor(0,40);
+    display.setTextColor(WHITE);
+    display.println(umbral);
+
+    display.display(); //Muestra el display
+  }
+
+  if (pantalla == 2)
+  {
+    display.clearDisplay();
+    display.setTextSize(2); //Establece el tamaño de texto
+
+    display.setCursor(0,10);
+    display.setTextColor(WHITE);
+    display.println(umbral);
+
+    display.display(); //Muestra el display
+  }
+  
+  //BOTONES
+  valau = digitalRead(SWAU);
+  valre = digitalRead(SWRE);
+
+  //MODIFICAR UMBRAL
+  if (valau == 0 && bloqau == 0 && pantalla == 2)
+  {
+    umbral = umbral + 1; //aumentar umbral
+  }
+
+  if (valre == 0 && bloqre == 0 && pantalla == 2)
+  {
+    umbral = umbral - 1; //reducir umbral
+  }
+  
+  //CAMBIAR PANTALLA
+  if (valau == 0 && valre == 0 && pantalla == 1 && cambiar == 0) //cambiar panatalla
+  {
+    Serial.println("pantalla 2");
+    cambiar = 1;
+    pantalla = 2;
+  } 
+
+  if (valau == 0 && valre == 0 && pantalla == 2 && cambiar == 0)
+  {
+    Serial.println("pantalla 1");
+    cambiar = 1;
+    pantalla = 1;
+  } 
+
+  //EVITAR QUE LOS BOTONES SE MANTENGAN EN FUNCIONAMIENTO
+  if (valau == 0)
+  {
+    bloqau = 1;
+  }
+  if (valau == 1)
+  {
+    bloqau = 0;
+  }
+
+  if (valre == 0)
+  {
+    bloqre = 1;
+  }
+  if (valre == 1)
+  {
+    bloqre = 0;
+  }
+
+  //Anti cambio de pantalla
+  if (valau == 1 && valre == 1)
+  {
+    cambiar = 0;
+  }
+
+  //PRENDER LED
+  if (temp < umbral)
     {
       digitalWrite(LED, LOW);
     }
 
-  if (temp >= 28)
+  if (temp >= umbral)
   {
     digitalWrite(LED, HIGH);
   }
